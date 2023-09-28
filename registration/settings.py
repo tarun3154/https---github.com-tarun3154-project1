9a2700d3efce860e0ddaf28a012da5d64bdd2e7d
Tarun Sharma <tarun315453@gmail.com>
2023-09-28 16:47:59 +0530

    2nd commit

--- registration/settings.py
+++ registration/settings.py
@@ -118,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Absolute filesystem path to the directory that will hold static files.
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
